a288c060d016b0f5eca79e34e8418882c043852c
snow-man999 <snowmandot0520@gmail.com>
2025-06-03 12:16:36 -0700

    Add files via upload

---
+++ api.php
@@ -0,0 +1,14 @@
+For clients interested in purchasing the Crazybets.cc pre-made template, we offer a fully functional online casino package. This includes:
+
+Over 800+ slot and live casino games
+8 house games All working and fixed games w/ Probability Fair
+Probability Fair with Verifier system
+Live sports betting
+7 pre-installed payment methods (customizable)
+Crypto deposit and withdrawal system
+Bonus system, referral program, Promotions system, user profiles, and much more!
+Contact us for access/purchase Crazybets.cc :
+
+Website: crazybets.cc
+Telegram: t.me/hesuse
+Discord: hesues
